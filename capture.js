c7e52e2a45e2e9591356ce389b0027026c3821a0
Ching-Wei Chen <chingweichen@gmail.com>
2015-06-19 10:38:52 +0200

    test

--- capture.js
+++ capture.js
@@ -58,7 +58,7 @@ function startup() {
         video.mozSrcObject = stream;
 
         // Connect the audio stream to MediaRecorder
-        mediaRecorder = new MediaRecorder(stream, {mimeType: "audio/ogg; codecs=vorbis"});
+        mediaRecorder = new MediaRecorder(stream);
         mediaRecorder.ondataavailable = function(evt) {
           chunks.push(evt.data);
         };
@@ -69,7 +69,7 @@ function startup() {
 
         mediaRecorder.onstop = function(evt) {
           console.log('onstop fired');
-          var blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=vorbis' });
+          var blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
           $('#status').text("Uploading...");
           upload(blob);
         };
